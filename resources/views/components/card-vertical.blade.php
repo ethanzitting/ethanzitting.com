623c651541a64c7905bf2d97ddf4fb9ef48093b8
Ethan Zitting <ethan@ethanzitting.com>
2023-03-24 10:30:31 -0500

    Fix broken deployment build
    Refine mobile styling of several blocks, commit images

--- resources/views/components/card-vertical.blade.php
+++ resources/views/components/card-vertical.blade.php
@@ -1,5 +1,5 @@
-<div class="card-vertical subtle-border accent-color">
+<li class="card-vertical subtle-border accent-color">
     @include($iconSrc)
     <h3 class="glossy-text">{{ $title }}</h3>
     <p>{{ $details }}</p>
-</div>
+</li>
